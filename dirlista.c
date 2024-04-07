9cba8e64420bd27c37cd244d2fdc6e6fda6da139
Nana <nanasmacbookprowithtouchbar@songruohandeMacBook-Pro.local>
2024-04-07 16:30:44 -0400

    Nana | sub-features | first three commands added

---
+++ dirlista.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char** getSubdirectories_alpha(int *count) {
+    FILE *fp;   // file pointer
+    char dir_name[1035];    // name of a directory
+    char **subdirs = NULL;  // array of pointers referring to all the subdirectories
+    int subdir_count = 0;   // number of subdirectories, initialized as 0
+
+    // Run the shell command and its output will be available for reading from 'fp'
+    // This command lists all the directories in the user's home directory
+    // strips their full path with only directory name left
+    // sorts them in a case-insensitive manner
+    fp = popen("ls -1d $HOME/*/ | xargs -n 1 basename | sort -f", "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Failed to run command\n");
+        return NULL;
+    }
+
+    // Read the output from 'fp' line by line
+    while (fgets(dir_name, sizeof(dir_name)-1, fp) != NULL) {
+        // Remove newline character
+        dir_name[strcspn(dir_name, "\n")] = 0;
+
+        // Change the size of the array of subdirectories
+        subdirs = realloc(subdirs, (subdir_count + 1) * sizeof(char *));
+        // Allocate memory for a new element of the subdirectories array
+        subdirs[subdir_count] = malloc(strlen(dir_name) + 1);
+
+        // Copy new subdirectory name read from 'fp' into the current array element
+        strcpy(subdirs[subdir_count], dir_name);
+        subdir_count++;
+    }
+
+    // Close the pipe and get the return code
+    int ret = pclose(fp);
+    if (ret == -1) {
+        fprintf(stderr, "Error while closing pipe\n");
+        return NULL;
+    }
+
+    *count = subdir_count;
+    return subdirs;
+}
+
+int main() {
+    int count;
+    char **subdirs = getSubdirectories_alpha(&count);
+    if (subdirs == NULL) {
+        fprintf(stderr, "Error: Failed to get subdirectories.\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("There are totally %d subdirectories in %s:\n", count, getenv("HOME"));
+    for (int i = 0; i < count; i++) {
+        printf("%s\n", subdirs[i]);
+        free(subdirs[i]);
+    }
+
+    free(subdirs);
+
+    return EXIT_SUCCESS;
+}
