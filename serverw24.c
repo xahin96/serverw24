e2aad3d48b04bcb15bdcd0fc3ee53a6704d8a5a4
Nana <nanasmacbookprowithtouchbar@songruohandeMacBook-Pro.local>
2024-04-09 19:00:26 -0400

    Nana | feature/dirlista | dirlist alphabetical updated in server and mirrors

--- serverw24.c
+++ serverw24.c
@@ -12,18 +12,76 @@
 int total_client = 0;
 int server_index = 0;
 
-void handle_dirlist_all(int conn) {
-    char message[200];
-    int num_messages = 0;
-    while (num_messages < 5) {
-        sleep(1);
-        sprintf(message, "message - %d", num_messages); // Create the message
-        send(conn, message, strlen(message), 0);
-        num_messages++;
+// Get the list of subdirectories under home directory in the alphabetical order
+char** getSubdirectories_alpha(int *count) {
+    FILE *fp;   // file pointer
+    char dir_name[1035];    // name of a directory
+    char **subdirs = NULL;  // array of pointers referring to all the subdirectories
+    int subdir_count = 0;   // number of subdirectories, initialized as 0
+
+    // Run the shell command and its output will be available for reading from 'fp'
+    // This command lists all the directories in the user's home directory
+    // strips their full path with only directory name left
+    // sorts them in a case-insensitive manner
+    fp = popen("ls -1d $HOME/*/ | xargs -n 1 basename | sort -f", "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Failed to run command\n");
+        return NULL;
     }
-    memset(message, 0, sizeof(message)); // Clear message buffer
+
+    // Read the output from 'fp' line by line
+    while (fgets(dir_name, sizeof(dir_name)-1, fp) != NULL) {
+        // Remove newline character
+        dir_name[strcspn(dir_name, "\n")] = 0;
+
+        // Change the size of the array of subdirectories
+        subdirs = realloc(subdirs, (subdir_count + 1) * sizeof(char *));
+        // Allocate memory for a new element of the subdirectories array
+        subdirs[subdir_count] = malloc(strlen(dir_name) + 1);
+
+        // Copy new subdirectory name read from 'fp' into the current array element
+        strcpy(subdirs[subdir_count], dir_name);
+        subdir_count++;
+    }
+
+    // Close the pipe and get the return code
+    int ret = pclose(fp);
+    if (ret == -1) {
+        fprintf(stderr, "Error while closing pipe !!!\n");
+        return NULL;
+    }
+
+    *count = subdir_count;
+    return subdirs;
+}
+
+int handle_dirlist_alpha(int conn) {
+    int num_messages;
+    char **subdirs = getSubdirectories_alpha(&num_messages);
+    if (subdirs == NULL) {
+        fprintf(stderr, "Error: Failed to get subdirectories.\n");
+        return EXIT_FAILURE;
+    }
+
+    char message[200];
+    sprintf(message, "There are %d subdirectories:\n", num_messages);
+    send(conn, message, strlen(message), 0);
+    // printf("There are totally %d subdirectories in %s:\n", num_messages, getenv("HOME"));
+    for (int i = 0; i < num_messages; i++) {
+        sleep(1);
+        send(conn, subdirs[i], strlen(subdirs[i]), 0);
+        // printf("%s\n", subdirs[i]);
+
+        // Clear message buffer
+        memset(subdirs[i], 0, sizeof(subdirs[i]));
+        free(subdirs[i]);
+    }
+    
     // Send termination message after sending all messages
     send(conn, "END_OF_MESSAGES", strlen("END_OF_MESSAGES"), 0);
+
+    free(subdirs);
+    return EXIT_SUCCESS;
 }
 
 // Function to handle client requests
@@ -37,7 +95,7 @@ void crequest(int conn, int server_port) {
             printf("Client message: %s\n", message);
 
             if (strstr(message, "dirlist -a") != NULL) {
-                handle_dirlist_all(conn);
+                handle_dirlist_alpha(conn);
             }
 
             // Check for exit condition
