3e16d07c66044bb24cdb73aed69a09fa0813a39f
zahin <xahin96@gmail.com>
2024-04-14 23:59:33 -0400

    Shahriar | client-command-verification | all the commands are now being validated before sending to server

--- clientw24.c
+++ clientw24.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
@@ -7,10 +8,42 @@
 #include <unistd.h>
 #include <ifaddrs.h>
 
+
 #define BUFFER_SIZE 32768
 
 
 #define DEST_FILE "./temp.tar.gz"
+
+// For counting the total number of space
+int special_space_count = 0;
+#define MAX_COMMAND_LENGTH 100
+
+// Splits a command with space and returns the sub command list
+char **split_by_space_operator(const char *command, const char *special_character) {
+    // restarting the space count variable
+    special_space_count = 0;
+    // variable for storing the temporary sub command
+    char *sub_command_string;
+    // Copying the command so that the main string doesn't get manipulated
+    char *command_copy = strdup(command);
+    // array for storing all the sub commands
+    char **commands = malloc(MAX_COMMAND_LENGTH * sizeof(char *));
+
+    // tokenizing the command_copy variable by special_character
+    sub_command_string = strtok(command_copy, special_character);
+    // looking for all the tokens for storing
+    while (sub_command_string != NULL) {
+        // inserting sub command to array
+        commands[special_space_count++] = strdup(sub_command_string);
+        // continuing the sub command splitting
+        sub_command_string = strtok(NULL, special_character);
+    }
+    // null terminating for ending the array
+    commands[special_space_count] = NULL;
+    // freeing the memory
+    free(command_copy);
+    return commands;
+}
 
 void handle_dirlist_alpha(int fd) {
     char message[100] = "";
@@ -232,6 +265,42 @@ void handle_w24fdb_before(int fd) {
     fclose(file);
 }
 
+bool is_leap_year(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+bool validate_date(char **specific_command) {
+    const char *date_str = specific_command[1];
+
+    int year, month, day;
+    if (sscanf(date_str, "%d-%d-%d", &year, &month, &day) != 3)
+        return false;
+
+    if (year < 999 || year > 9999)
+        return false;
+
+    if (month < 1 || month > 12)
+        return false;
+
+    int days_in_month;
+    switch (month) {
+        case 2:
+            days_in_month = 28 + is_leap_year(year);
+            break;
+        case 4: case 6: case 9: case 11:
+            days_in_month = 30;
+            break;
+        default:
+            days_in_month = 31;
+    }
+
+    if (day < 1 || day > days_in_month)
+        return false;
+
+
+    return true;
+}
+
 int main() {
     struct sockaddr_in serv;
     int fd;
@@ -292,24 +361,70 @@ int main() {
             send(fd, message, strlen(message), 0);
             handle_dirlist_time(fd);
         } else if (strstr(message, "w24fn") != NULL) {
-            send(fd, message, strlen(message), 0);
-            handle_w24fn_filename(fd);
+            char **specific_command = split_by_space_operator(message, " ");
+            if (special_space_count == 2) {
+                send(fd, message, strlen(message), 0);
+                handle_w24fn_filename(fd);
+                free(specific_command);
+            } else {
+                printf("Invalid 'w24fn' command. Command structure:\nw24fn [file name]\n");
+                free(specific_command);
+            }
         } else if (strstr(message, "w24fz") != NULL) {
-            send(fd, message, strlen(message), 0);
-            handle_w24fz_size(fd);
+            int min_search_size;
+            int max_search_size;
+            char **specific_command = split_by_space_operator(message, " ");
+            if (special_space_count == 3) {
+                min_search_size = atoi(specific_command[1]);
+                max_search_size = atoi(specific_command[2]);
+                if (min_search_size >= 0 && max_search_size >= 0) {
+                    if (min_search_size <= max_search_size){
+                        send(fd, message, strlen(message), 0);
+                        handle_w24fz_size(fd);
+                    } else {
+                        printf("size2 must be greater than or equal to size1\n");
+                    }
+                } else {
+                    printf("size1 and size2 must be greater than or equal to zero\n");
+                }
+                free(specific_command);
+            } else {
+                printf("Invalid 'w24fz' command. Command structure:\nw24fz [size1] [size2]\n");
+                free(specific_command);
+            }
         } else if (strstr(message, "w24ft") != NULL) {
-            send(fd, message, strlen(message), 0);
-            handle_w24ft_ext(fd);
+            char **specific_command = split_by_space_operator(message, " ");
+            if (special_space_count >= 2 && special_space_count <= 4) {
+                send(fd, message, strlen(message), 0);
+                handle_w24ft_ext(fd);
+                free(specific_command);
+            } else {
+                printf("Invalid 'w24ft' command. Command structure:\nw24ft [file extension 1]\nor\nw24ft [file extension 1] [file extension 2]\nor\nw24ft [file extension 1] [file extension 2] [file extension 3]\n");
+                free(specific_command);
+            }
         } else if (strstr(message, "w24fda") != NULL) {
-            send(fd, message, strlen(message), 0);
-            handle_w24fda_after(fd);
+            char **specific_command = split_by_space_operator(message, " ");
+            if (special_space_count == 2 && validate_date(specific_command)) {
+                send(fd, message, strlen(message), 0);
+                handle_w24fda_after(fd);
+            } else {
+                printf("Invalid 'w24fda' command. Command structure:\nw24fda [Date(yyyy-mm-dd)]\n\n");
+                free(specific_command);
+            }
         } else if (strstr(message, "w24fdb") != NULL) {
-            send(fd, message, strlen(message), 0);
-            handle_w24fdb_before(fd);
+            char **specific_command = split_by_space_operator(message, " ");
+            if (special_space_count == 2 && validate_date(specific_command)) {
+                send(fd, message, strlen(message), 0);
+                handle_w24fdb_before(fd);
+            } else {
+                printf("Invalid 'w24fdb' command. Command structure:\nw24fdb [Date(yyyy-mm-dd)]\n\n");
+                free(specific_command);
+            }
         } else if (strncmp(message, "quitc", 5) == 0) {
+            // TODO: must send quitc to server
             break;
         } else {
-            send(fd, message, strlen(message), 0);
+            printf("Invalid command\n");
         }
     }
 
